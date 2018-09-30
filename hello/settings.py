d57eb02b91f3b339ca7597270be2dc3705924fc8
Chong Kim <chong@gridium.com>
2018-09-29 21:37:13 -0500

    webpack-dev-server

--- hello/settings.py
+++ hello/settings.py
@@ -118,5 +118,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'http://127.0.0.1:8080/'
 STATICFILES_DIRS = ['dist']
